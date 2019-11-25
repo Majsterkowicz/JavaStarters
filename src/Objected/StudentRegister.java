14071473cfcf618d3d0c14a5d3b385e30c049c62
Majsterkowicz <lis.lukasz92@gmail.com>
2019-11-25 10:33:55 +0100

    new program added

--- src/Objected/StudentRegister.java
+++ src/Objected/StudentRegister.java
@@ -4,7 +4,7 @@ public class StudentRegister {
     public static void main(String[] args) {
         Student student1 = new Student("Karol", "Kasztaniak" );
     }
-    show
+    
 
     private static void showStudentData(Student student) {
         System.out.println(student.getName() + " " + student.getSurname());
