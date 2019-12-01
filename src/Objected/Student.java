ae018aac67a9566c5d1e0083db2303d1f0ecfacc
Majsterkowicz <lis.lukasz92@gmail.com>
2019-12-01 17:44:07 +0100

    An "average" constructor of Object Student is added. Mark average getter is added, bot not working properly. Some crazy things happened. But our team is working on it.

--- src/Objected/Student.java
+++ src/Objected/Student.java
@@ -6,9 +6,10 @@ public class Student {
     private String name;
     private String surname;
     private int[] marks;
+    private double average;
 
-    public Student(String dupa, String surname, int[] marks) {
-        name = dupa;
+    public Student(String studentName, String surname, int[] marks) {
+        name = studentName;
         this.surname = surname;
         this.marks = marks;
     }
@@ -25,6 +26,18 @@ public class Student {
         return marks;
     }
 
+    public double getAverage() {
+        int i;
+        double sum = 0;
+        double average;
+        for (i = 0, i < marks.length, i++); {
+
+            sum += marks[i];
+        }
+        average = sum / i;
+        return average;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -36,8 +49,9 @@ public class Student {
     public void setMarks(int[] marks) {
         this.marks = marks;
     }
+
     public void showStudentData() {
-        System.out.println(name + " " + surname + " " + Arrays.toString(marks));
+        System.out.println(name + " " + surname + " " + Arrays.toString(marks) + average);
     }
 
 }
