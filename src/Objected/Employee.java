b5abd797232100167f5862131719f48cc2caa16b
Majsterkowicz <lis.lukasz92@gmail.com>
2020-01-27 20:49:48 +0100

    Class Employee splited for two classes. One Employee and second EmployeeRegister. Getters and setters are typed correctly and works well. On Register, main program is set up. Code ask for number of employees to add, then make an array of typed number. Then loop is started and ask for name, surname and salary for each employee. For last run, method for setting ID. Out of loop is statement with end of employees. Next step show all eployees data using getters from Employee class. Line with data using String.format to make code more clear and easy to read'n'change. Last one is method to show next avaliable ID for Employee.

---
+++ src/Objected/Employee.java
@@ -0,0 +1,52 @@
+package Objected;
+
+class Employee {
+    private String name;
+    private String surname;
+    private double salary;
+    private int id;
+    private static int nextId = 1;
+
+    public Employee(String name, String surname, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setId() {
+        id = nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- src/Objected/EmployeeRegister.java
+++ src/Objected/EmployeeRegister.java
@@ -1,16 +1,16 @@
 package Objected;
 
 import java.util.Scanner;
-//TODO dlaczego dwie klasy w jednym pliku, możesz to rozbic na 2 pliki, kazdy ze swoją klasą
+
 public class EmployeeRegister {
 
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int numberOfEployees = setNumberOfEmployees(); //TODO literówka w deklaracji zmiennej
-        Employee[] staff = new Employee[numberOfEployees];
-        setEmployeeData(staff);
-        //TODO dodaj na koncu metodę która wyswietli dane pracowników
+        int numberOfEmployees = setNumberOfEmployees();
+        Employee[] staff = createEmployees(numberOfEmployees);
+        showEmployeesData(staff);
+        showNextId();
     }
 
     private static int setNumberOfEmployees() {
@@ -18,65 +18,36 @@ public class EmployeeRegister {
         return scanner.nextInt();
     }
 
-    //TODO metoda ta powinna utworzyć dane pracowników (wywołac tyle razy konstruktor kalsy Employee ile wynosi zmienna numberOfEployees)
-    //TODO potem te dane zapisujesz do tablicy staff
-    private static void setEmployeeData(Employee[] employees) {
-        //tODO to id jest zupełnie zbędne, konstruktor kalsy Employee powinien dbac o nadanie odpowiedniego id (albo setter)
-        employees[id].setName();
+    private static Employee[] createEmployees(int numberOfEmployees) {
+        Employee[] staff = new Employee[numberOfEmployees];
+        for (int i = 0; i < staff.length; i++) {
+            System.out.println(String.format("Podaj dane pracownika %s", i + 1));
+            System.out.println("Podaj imię: ");
+            String name = scanner.next();
+            System.out.println("Podaj nazwisko: ");
+            String surname = scanner.next();
+            System.out.println("Podaj pensję: ");
+            double salary = scanner.nextDouble();
+
+            Employee employee = new Employee(name, surname, salary);
+            employee.setId();
+            staff[i] = employee;
+        }
+        System.out.println("Wprowadzono wszystkich pracowników.");
+        return staff;
     }
 
-    private static void getEmployeeData(Employee[] employees, int id) {
-        employees[id].getName();
-        employees[id].getSurname();
-        employees[id].getSalary();
+    private static void showEmployeesData(Employee[] staff) {
+        for (int i = 0; i < staff.length; i++) {
+            String name = staff[i].getName();
+            String surname = staff[i].getSurname();
+            double salary = staff[i].getSalary();
+            int id = staff[i].getId();
+            System.out.println(String.format("ID: %s. pracownik: %s %s, zarabia %s", id, name, surname, salary));
+        }
     }
 
-}
-
-class Employee {
-    private String name;
-    private String surname;
-    private double salary;
-    private int id;
-    private static int nextId = 1;
-
-    public Employee(String name, String surname, double salary) {
-        this.name = name;
-        this.surname = surname;
-        this.salary = salary;
-        id = 0; //TODO pracownikowi nie ustawiasz id = 0 tylko to którego stan przetrzymujesz w nextId, mozesz spróbować w tym miejscu wywołać metodę setId, wygląda dobrze (ustawia id a potem je ikrementuje)
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public void setId(int id) {
-        id = nextId;
-        nextId++;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public static int getNextId() {
-        return nextId;
+    private static void showNextId(){
+        System.out.println(String.format("Następny dostępny ID: %s", Employee.getNextId()));
     }
 }
