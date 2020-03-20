dffebcd24046cd669ae11b34b1df056adcc7e18c
Bartłomiej Olszówka <olszowka.bartlomiej@gmail.com>
2020-03-20 19:12:20 +0100

    collection exercises

---
+++ src/collections/ListExercises.java
@@ -0,0 +1,22 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListExercises {
+
+    public static void main(String[] args) {
+        List<Integer> numbers  = Arrays.asList(1,4,5,54,15,0);
+        //1. wyciagnij z listy liczby parzyste
+        //2. posortuj liste rosnaco/malejaco - poszukaj rozwiazania w internetach (na kilka sposobów)
+
+        List<String> names = Arrays.asList("Adam", "Stefan", "Marian", "Antoni", "Agnieszka", "James");
+        //1. wyciagnij listę imion zaczynajacych sie na litere 'A' i podaj ich liczbe
+        //2. wyciagnij listę imion dłuższych skaldajacych sie z wiecej niz 5 liter
+        //3. sprawdz czy lista zawiera imię "Antoni"
+
+        // przy uzyciu listy napisz program ktory sprawdza czy wyraz czytany od tyłu brzmi tak samo np. kajak, potop, mars .... nie mars nie
+
+
+    }
+}
---
+++ src/collections/MapExercises.java
@@ -0,0 +1,18 @@
+package collections;
+
+public class MapExercises {
+
+    public static void main(String[] args) {
+        //stworz mape w ktorej kluczem bedzie nazwa państwa a wartości kontynent (kilka par)
+        //wyswietl kontynent dla danego kraju
+        // wyswietl wszystkie panstwa
+        // wyswietl wszystkie kontynenty (bez powtórzen)
+        // sprawdz czy mapa zawiera Polskę
+        // podaj rozmiar mapy
+        // wyswietl wszystkie państwa alfabetycznie
+        // znajdz kontynent dla którego mamy najwiecej panstw w mapie
+        // dodaj listę z kontynentami które chcesz wyszukac. następnei sprawdz ile razy kazdy kontynent z danej listy wystepuje w mapie
+
+
+    }
+}
